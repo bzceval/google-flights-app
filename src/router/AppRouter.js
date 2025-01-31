1e275e9a666356fc9d15c8c8f1784788db655c57
busra <busraaceval@gmail.com>
2025-01-31 16:08:57 +0300

    ADD: home route

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "../components/Footer";
+import Home from "../pages/Home";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
