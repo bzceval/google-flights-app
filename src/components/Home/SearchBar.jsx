0a7718108ed08d3436ddc06d7c777b0a1f66cc9f
busra <busraaceval@gmail.com>
2025-02-02 04:18:30 +0300

    ADD: round trip search bar

--- src/components/Home/SearchBar.jsx
+++ src/components/Home/SearchBar.jsx
@@ -9,6 +9,7 @@ import {
   MenuItem,
   Grid,
   useTheme,
+  Divider,
 } from "@mui/material";
 import {
   SwapHoriz as SwapHorizIcon,
@@ -178,19 +179,70 @@ const SearchBar = () => {
           />
         </Grid>
         <Grid item xs={12} sm={6} md={5}>
-          <TextField
-            fullWidth
-            type="date"
-            variant="outlined"
-            InputProps={{
-              startAdornment: <CalendarMonthOutlinedIcon sx={{ mr: 1 }} />,
-            }}
+          <Box
             sx={{
-              borderRadius: 1,
-              color: "#fff",
-              input: { color: "#fff" },
+              display: "flex",
+              border: `1px solid ${theme.palette.mainColors.border}`,
+              borderRadius: "5px",
+              ":hover": {
+                border: `1px solid ${theme.palette.mainColors.primary}`,
+              },
+              transition: "border 0.3s ease, box-shadow 0.3s ease",
             }}
-          />
+          >
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="Departure"
+              InputProps={{
+                startAdornment: <CalendarMonthOutlinedIcon sx={{ mr: 1 }} />,
+              }}
+              sx={{
+                borderRadius: 1,
+                color: "#fff",
+                input: { color: "#fff" },
+                "& .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "transparent",
+                },
+                "&:hover .MuiOutlinedInput-notchedOutline": {
+                  borderColor: theme.palette.mainColors.primary,
+                },
+                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "#fff",
+                },
+                transition: "border 0.3s ease",
+              }}
+            />
+            <Divider
+              orientation="vertical"
+              flexItem
+              sx={{
+                bgcolor: theme.palette.mainColors.border,
+                height: "35px",
+                alignSelf: "center",
+              }}
+            />
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="Return"
+              sx={{
+                borderRadius: 1,
+                color: "#fff",
+                input: { color: "#fff" },
+                "& .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "transparent",
+                },
+                "&:hover .MuiOutlinedInput-notchedOutline": {
+                  borderColor: theme.palette.mainColors.primary,
+                },
+                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "#fff",
+                },
+                transition: "border 0.3s ease",
+              }}
+            />
+          </Box>
         </Grid>
       </Grid>
 
